8393d0ea658e22e1ba2213ff4b19e3a1f8147034
EileenIL <114991042+EileenIL@users.noreply.github.com>
2024-02-11 13:56:11 +0700

    Add files via upload

---
+++ Project1.h
@@ -0,0 +1,65 @@
+﻿#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for About
+	/// </summary>
+	public ref class About : public System::Windows::Forms::Form
+	{
+	public:
+		About(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~About()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// About
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1044, 482);
+			this->Name = L"About";
+			this->Text = L"About";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
