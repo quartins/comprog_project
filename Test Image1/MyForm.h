517f4c96ecc96d59172f5aac30612071306844f4
PraeThipwarin <141800070+PraeThipwarin@users.noreply.github.com>
2024-02-15 08:44:16 +0700

    TestImage1_2

---
+++ Test Image1/MyForm.h
@@ -0,0 +1,151 @@
+#pragma once
+
+namespace TestImage1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.BackgroundImage")));
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button1->Location = System::Drawing::Point(252, 623);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(637, 225);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Click";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
+			this->pictureBox1->Location = System::Drawing::Point(27, 28);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(534, 528);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 1;
+			this->pictureBox1->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Location = System::Drawing::Point(597, 28);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(561, 528);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 2;
+			this->pictureBox2->TabStop = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->ClientSize = System::Drawing::Size(1258, 926);
+			this->Controls->Add(this->pictureBox2);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+		int image1,image2;
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		Random^ random = gcnew Random();
+		image1 = random->Next(1, 6);\
+			switch (image1){
+			case 1:
+				this->pictureBox1->Load("01-cups.png");
+				break;
+			case 2:
+				this->pictureBox1->Load("02-cups.png");
+				break;
+			case 3:
+				this->pictureBox1->Load("03-cups.png");
+				break;
+			case 4:
+				this->pictureBox1->Load("04-cups.png");
+				break;
+			case 5:
+				this->pictureBox1->Load("05-cups.png");
+				break;
+			}
+		image2 = random->Next(1, 6); \
+			switch (image2) {
+			case 1:
+				this->pictureBox2->Load("01-cups.png");
+				break;
+			case 2:
+				this->pictureBox2->Load("02-cups.png");
+				break;
+			case 3:
+				this->pictureBox2->Load("03-cups.png");
+				break;
+			case 4:
+				this->pictureBox2->Load("04-cups.png");
+				break;
+			case 5:
+				this->pictureBox2->Load("05-cups.png");
+				break;
+			}
+		
+	}
+	};
+}
