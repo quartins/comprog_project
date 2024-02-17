29f9daaa19a35a36da284c1433a0dc012178bd8b
nani <chotima.nani@gmail.com>
2024-02-17 19:53:46 +0700

    completed

---
+++ Project/Power6rangers/page.h
@@ -0,0 +1,83 @@
+#pragma once
+
+namespace Power6rangers {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for page
+	/// </summary>
+	public ref class page : public System::Windows::Forms::Form
+	{
+	public:
+		page(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~page()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(page::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Sylfaen", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(463, 261);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(192, 73);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"start";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// page
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(1120, 557);
+			this->Controls->Add(this->button1);
+			this->Name = L"page";
+			this->Text = L"page";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
