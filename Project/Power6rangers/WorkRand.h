308c74f9c1b316cbedaec0992c624bde123a5250
Sadanun <sadanun.en@gmail.com>
2024-02-17 19:38:23 +0700

    Work

---
+++ Project/Power6rangers/WorkRand.h
@@ -0,0 +1,89 @@
+﻿#pragma once
+#include "WorkShow.h"
+namespace Power6rangers {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for WorkRand
+	/// </summary>
+	public ref class WorkRand : public System::Windows::Forms::Form
+	{
+	public:
+		WorkRand(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~WorkRand()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(WorkRand::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(543, 357);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(110, 68);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"สุ่ม";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &WorkRand::button1_Click);
+			// 
+			// WorkRand
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->ClientSize = System::Drawing::Size(1221, 483);
+			this->Controls->Add(this->button1);
+			this->Name = L"WorkRand";
+			this->Text = L"WorkRand";
+			this->Load += gcnew System::EventHandler(this, &WorkRand::WorkRand_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void WorkRand_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
