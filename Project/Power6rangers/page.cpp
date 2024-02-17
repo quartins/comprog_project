cb79e941bce625281b62e8ca6aa995d00e818c40
PraeThipwarin <prae.tippy@gmail.com>
2024-02-17 21:23:16 +0700

    page.cpp edited

--- Project/Power6rangers/page.cpp
+++ Project/Power6rangers/page.cpp
@@ -1,12 +1,2 @@
 #include "page.h"
 
-using namespace System;
-using namespace System::Windows::Forms;
-
-[STAThread]
-void main(array<String^>^ args) {
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-    Power6rangers::page form;
-    Application::Run(% form);
-}
