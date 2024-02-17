999f9f538fa8ac6cb2c0040116c13d76e156cb36
PraeThipwarin <prae.tippy@gmail.com>
2024-02-17 16:13:58 +0700

    Daily Create

---
+++ Project/Power6rangers/Page0.cpp
@@ -0,0 +1,9 @@
+#include "Page0.h"
+
+using namespace Power6rangers;
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew Page0());
+}
