ace79547e4b4c92bd5d9e3ff4f52e2db7f11fe82
Sadanun <sadanun.en@gmail.com>
2024-02-17 21:46:48 +0700

    edit code

--- Project/Power6rangers/WorkShow.cpp
+++ Project/Power6rangers/WorkShow.cpp
@@ -1,2 +1,16 @@
 ﻿#include "WorkShow.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Power6rangers::WorkShow form;
+	Application::Run(% form);
+}
+
+
+
+
