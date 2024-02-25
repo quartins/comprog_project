4153f107dd25121d889b94fc888c61270a5af352
Jjjjaoant <thanchanok_naensin@cmu.ac.th>
2024-02-25 14:52:18 +0700

    Love 2 complete (ver.no ok)

--- com_prog/ProjectPower6rangers/ProjectPower6rangers/LoveShow.h
+++ com_prog/ProjectPower6rangers/ProjectPower6rangers/LoveShow.h
@@ -34,6 +34,8 @@ namespace ProjectPower6rangers {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,11 +50,35 @@ namespace ProjectPower6rangers {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"LoveShow";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(LoveShow::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->button1->BackColor = System::Drawing::Color::LightPink;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(1117, 425);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(170, 49);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"BACK";
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// LoveShow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1382, 501);
+			this->Controls->Add(this->button1);
+			this->Name = L"LoveShow";
+			this->Text = L"LoveShow";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
